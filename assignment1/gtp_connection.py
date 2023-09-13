ed90c998f4c1f6706827d411776c8c6d74eb8140
cubeof2 <cubeof2@gmail.com>
2023-09-13 16:21:22 -0600

    implement end game genmove cmd

--- assignment1/gtp_connection.py
+++ assignment1/gtp_connection.py
@@ -347,7 +347,15 @@ class GtpConnection:
         move = self.go_engine.get_move(self.board, color)
         move_coord = point_to_coord(move, self.board.size)
         move_as_string = format_point(move_coord)
-        if self.board.is_legal(move, color):
+
+        # add end of game logic
+        if self.board.end_of_game():
+            if not self.board.get_empty_points().any():
+                self.respond("pass")
+            else:
+                self.respond("resign")
+
+        elif self.board.is_legal(move, color):
             self.board.play_move(move, color)
             self.respond(move_as_string)
         else:
